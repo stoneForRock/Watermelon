b381c22ffc5379206178a290fd92e289939bb31b
shichuang <597676784@qq.com>
2019-04-08 18:20:36 +0800

    添加频道页面请求

---
+++ WatermelonVedioPlayer/WatermelonVedioPlayer/Modules/Channel/Network/ChannelRequest.h
@@ -0,0 +1,56 @@
+//
+//  ChannelRequest.h
+//  WatermelonVedioPlayer
+//
+//  Created by dachen on 2019/4/8.
+//  Copyright © 2019年 VedioPlayer. All rights reserved.
+//
+
+#import "BaseRequest.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChannelRequest : BaseRequest
+
+/**
+ 获取频道标签列表
+
+ @param finishBlock finishBlock
+ */
++ (void)getTagListFinishBlock:(RequestFinishBlock)finishBlock;
+
+/**
+ 根据标签查询电影
+
+ @param tagIds 标签列表
+ @param page 当前页
+ @param finishBlock finishBlock
+ */
++ (void)queryMovieWithTagIds:(NSArray *)tagIds page:(NSInteger)page finishBlock:(RequestFinishBlock)finishBlock;
+
+/**
+ 获取演员分类列表
+
+ @param finishBlock finishBlock
+ */
++ (void)getActorCategoriesFinishBlock:(RequestFinishBlock)finishBlock;
+
+/**
+ 演员列表
+
+ @param categoryId 演员分类id
+ @param sort sort – 排序类型（可选值movie_count 影片量, play_count 人气，默认 play_count）
+ @param finishBlock finishBlock
+ */
++ (void)getActorListWithCategoryId:(NSString *)categoryId sort:(NSString *)sort finishBlock:(RequestFinishBlock)finishBlock;
+
+/**
+ 获取所有的人气演员
+
+ @param finishBlock finishBlock
+ */
++ (void)getHotsActorFinishBlock:(RequestFinishBlock)finishBlock;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WatermelonVedioPlayer/WatermelonVedioPlayer/Modules/Channel/Network/ChannelRequest.m
@@ -0,0 +1,43 @@
+//
+//  ChannelRequest.m
+//  WatermelonVedioPlayer
+//
+//  Created by dachen on 2019/4/8.
+//  Copyright © 2019年 VedioPlayer. All rights reserved.
+//
+
+#import "ChannelRequest.h"
+
+@implementation ChannelRequest
+
++ (void)getTagListFinishBlock:(RequestFinishBlock)finishBlock {
+    [self sendGETRequest:@"/api/tag/list" parameters:@{} callBack:^(BOOL success, id  _Nullable responseObject, NSError * _Nullable error) {
+        finishBlock(success,responseObject,error);
+    }];
+}
+
++ (void)queryMovieWithTagIds:(NSArray *)tagIds page:(NSInteger)page finishBlock:(RequestFinishBlock)finishBlock {
+    [self sendPOSTRequest:@"/api/movie/bytags" parameters:@{@"tagIds":tagIds,@"page":@(page),@"pageSize":@"10"} callBack:^(BOOL success, id  _Nullable responseObject, NSError * _Nullable error) {
+        finishBlock(success,responseObject,error);
+    }];
+}
+
++ (void)getActorCategoriesFinishBlock:(RequestFinishBlock)finishBlock {
+    [self sendGETRequest:@"/api/actor/categories" parameters:@{} callBack:^(BOOL success, id  _Nullable responseObject, NSError * _Nullable error) {
+        finishBlock(success,responseObject,error);
+    }];
+}
+
++ (void)getActorListWithCategoryId:(NSString *)categoryId sort:(NSString *)sort finishBlock:(RequestFinishBlock)finishBlock {
+    [self sendGETRequest:@"/api/actor/list" parameters:@{@"categoryId":categoryId,@"sort":sort} callBack:^(BOOL success, id  _Nullable responseObject, NSError * _Nullable error) {
+        finishBlock(success,responseObject,error);
+    }];
+}
+
++ (void)getHotsActorFinishBlock:(RequestFinishBlock)finishBlock {
+    [self sendGETRequest:@"/api/actor/hots" parameters:@{} callBack:^(BOOL success, id  _Nullable responseObject, NSError * _Nullable error) {
+        finishBlock(success,responseObject,error);
+    }];
+}
+
+@end
